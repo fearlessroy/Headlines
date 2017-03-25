c99774c16519f05a5955820f4e276dd7fe8ba6fe
yunfeiroy666 <yunfeiroy@gmail.com>
2017-03-25 10:18:39 +0800

    add meaven

--- src/main/java/com/wyf/HeadlineApplication.java
+++ src/main/java/com/wyf/HeadlineApplication.java
@@ -2,11 +2,20 @@ package com.wyf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
+//public class HeadlineApplication extends SpringBootServletInitializer {
 public class HeadlineApplication {
+    /*
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HeadlineApplication.class);
+    }*/
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeadlineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HeadlineApplication.class, args);
+    }
+
 }
